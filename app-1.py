23b5a75ce56ac29137f2edf67d9ab2260192478e
saurabhshcs <saurabhshcs@gmail.com>
2022-10-28 01:04:58 +0100

    Updated for docker swarm

--- app-1.py
+++ app-1.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Beautiful World!"
-
+    return "hello world!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
